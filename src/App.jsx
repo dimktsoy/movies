8bb4e5efa62c51e7f060425d493988e7e9a76f7c
Dima Tsoy <dimktsoy@yahoo.com>
2022-08-22 07:51:54 +0600

    Реализация компонента App через class

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,50 @@
+import React from 'react';
 import { Layout } from 'antd';
-import PageHeader from './components/layout/PageHeader';
-import PageFooter from './components/layout/PageFooter';
-import PageContent from './components/layout/PageContent';
 
-function App() {
-  return (
-    <Layout>
-      <PageHeader />
-      <PageContent>Content</PageContent>
-      <PageFooter />
-    </Layout>
-  );
+import PageHeader from './layout/PageHeader';
+import PageFooter from './layout/PageFooter';
+import PageContent from './layout/PageContent';
+import MovieList from './components/MovieList';
+import SearchForm from './components/SearchForm';
+
+class App extends React.Component {
+  state = {
+    movies: [],
+  };
+
+  async componentDidMount() {
+    try {
+      const response = await fetch(
+        'http://www.omdbapi.com/?apikey=fded1e22&s=Naruto'
+      );
+      console.log(response, 'response');
+      const { Search, Response, Error: errorMessage } = await response.json();
+
+      if (Response === 'False') {
+        throw Error(errorMessage);
+      }
+
+      this.setState({ movies: Search });
+    } catch (error) {
+      console.log(error, 'error');
+    } finally {
+    }
+  }
+
+  render() {
+    const { movies } = this.state;
+
+    return (
+      <Layout>
+        <PageHeader />
+        <PageContent>
+          <SearchForm />
+          <MovieList movies={movies} />
+        </PageContent>
+        <PageFooter />
+      </Layout>
+    );
+  }
 }
 
 export default App;
