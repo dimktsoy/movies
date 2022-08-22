3633f9bd1ff0a979aa19c0ea53d3ba068e944f74
Dima Tsoy <dtsoi@cic.kz>
2022-08-22 17:47:53 +0600

    Обернул App в BrowserRouter, добавил страницу Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,101 +1,26 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 import Page from './layout/Page';
 import PageHeader from './layout/PageHeader';
 import PageFooter from './layout/PageFooter';
 import PageContent from './layout/PageContent';
-import MovieList from './components/MovieList';
-import SearchForm from './components/SearchForm';
-import Loader from './components/Loader';
-import PaginationPanel from './components/PaginationPanel';
+import HomePage from './pages/Home';
+import MoviePage from './pages/Movie';
 
-class App extends React.Component {
-  state = {
-    movies: [],
-    isLoading: true,
-    totalResults: 0,
-    search: {
-      type: 'all',
-      title: 'Matrix',
-    },
-  };
-
-  searchMovie = async (page = 1) => {
-    try {
-      this.setState({ isLoading: true });
-
-      const response = await fetch(
-        `http://www.omdbapi.com/?apikey=fded1e22&s=${this.state.search.title}${
-          this.state.search.type === 'all'
-            ? ''
-            : `&type=${this.state.search.type}`
-        }&page=${page}`
-      );
-
-      const {
-        Search,
-        Response,
-        totalResults,
-        Error: errorMessage,
-      } = await response.json();
-
-      if (Response === 'False') {
-        throw Error(errorMessage);
-      }
-
-      this.setState({
-        movies: Search,
-        totalResults: parseInt(totalResults, 10),
-      });
-    } catch (error) {
-      this.setState({ movies: [], totalResults: 0 });
-    } finally {
-      this.setState({ isLoading: false });
-    }
-  };
-
-  handleChangeTitle = (event) =>
-    this.setState({
-      search: {
-        ...this.state.search,
-        title: event.target.value,
-      },
-    });
-
-  handleChangeType = (event) =>
-    this.setState({
-      search: { ...this.state.search, type: event.target.value },
-    });
-
-  async componentDidMount() {
-    await this.searchMovie();
-  }
-
-  render() {
-    const { movies, isLoading, totalResults, search } = this.state;
-
-    return (
-      <Page>
-        <PageHeader />
-        <PageContent>
-          <SearchForm
-            onSearchMovie={this.searchMovie}
-            onChangeTitle={this.handleChangeTitle}
-            onChangeType={this.handleChangeType}
-            isLoading={isLoading}
-            title={search.title}
-            type={search.type}
-          />
-          {isLoading ? <Loader /> : <MovieList movies={movies} />}
-          <PaginationPanel
-            total={totalResults}
-            onSearchMovie={this.searchMovie}
-          />
-        </PageContent>
-        <PageFooter />
-      </Page>
-    );
-  }
+function App() {
+  return (
+    <Page>
+      <PageHeader />
+      <PageContent>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/:movieID" element={<MoviePage />} />
+        </Routes>
+      </PageContent>
+      <PageFooter />
+    </Page>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import Loader from '../components/Loader';
+import MovieList from '../components/MovieList';
+import PaginationPanel from '../components/PaginationPanel';
+import SearchForm from '../components/SearchForm';
+
+class HomePage extends React.Component {
+  state = {
+    movies: [],
+    isLoading: true,
+    totalResults: 0,
+    search: {
+      type: 'all',
+      title: 'Matrix',
+    },
+  };
+
+  searchMovie = async (page = 1) => {
+    try {
+      this.setState({ isLoading: true });
+
+      const response = await fetch(
+        `http://www.omdbapi.com/?apikey=fded1e22&s=${this.state.search.title}${
+          this.state.search.type === 'all'
+            ? ''
+            : `&type=${this.state.search.type}`
+        }&page=${page}`
+      );
+
+      const {
+        Search,
+        Response,
+        totalResults,
+        Error: errorMessage,
+      } = await response.json();
+
+      if (Response === 'False') {
+        throw Error(errorMessage);
+      }
+
+      this.setState({
+        movies: Search,
+        totalResults: parseInt(totalResults, 10),
+      });
+    } catch (error) {
+      this.setState({ movies: [], totalResults: 0 });
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  };
+
+  handleChangeTitle = (event) =>
+    this.setState({
+      search: {
+        ...this.state.search,
+        title: event.target.value,
+      },
+    });
+
+  handleChangeType = (event) =>
+    this.setState({
+      search: { ...this.state.search, type: event.target.value },
+    });
+
+  async componentDidMount() {
+    await this.searchMovie();
+  }
+
+  render() {
+    const { movies, isLoading, totalResults, search } = this.state;
+
+    return (
+      <>
+        <SearchForm
+          onSearchMovie={this.searchMovie}
+          onChangeTitle={this.handleChangeTitle}
+          onChangeType={this.handleChangeType}
+          isLoading={isLoading}
+          title={search.title}
+          type={search.type}
+        />
+        {isLoading ? <Loader /> : <MovieList movies={movies} />}
+        <PaginationPanel
+          total={totalResults}
+          onSearchMovie={this.searchMovie}
+        />
+      </>
+    );
+  }
+}
+
+export default HomePage;
