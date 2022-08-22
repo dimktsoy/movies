04579812c36869fc8e1a288c08302c876c7a1905
Dima Tsoy <dtsoi@cic.kz>
2022-08-22 16:38:20 +0600

    Заглушка для пустого конента

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,7 +1,11 @@
-import { Col, Row } from 'antd';
+import { Col, Empty, Row } from 'antd';
 import MovieItem from './MovieItem';
 
 function MovieList({ movies = [] }) {
+  if (!movies.length) {
+    return <Empty description="Nothing found" />;
+  }
+
   return (
     <Row gutter={[30, 30]} style={{ marginBlock: '30px' }}>
       {movies.map((movie) => (
