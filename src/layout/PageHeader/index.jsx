52bd969386a4eab495de45a284bcd1d2c2cd93d7
dimktsoy <dimktsoy@yahoo.com>
2022-08-23 11:34:24 +0600

    Ссылка на главную в шапке

--- src/layout/PageHeader/index.jsx
+++ src/layout/PageHeader/index.jsx
@@ -1,8 +1,13 @@
 import { Header } from 'antd/lib/layout/layout';
+import { NavLink } from 'react-router-dom';
 import './PageHeader.scss';
 
 function PageHeader() {
-  return <Header className="page-header">Header</Header>;
+  return (
+    <Header className="page-header">
+      <NavLink to="/">Header</NavLink>
+    </Header>
+  );
 }
 
 export default PageHeader;
