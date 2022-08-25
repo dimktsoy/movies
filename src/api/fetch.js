15490be606c323da3468b0da26a69b799df5bd6a
dimktsoy <dimktsoy@yahoo.com>
2022-08-25 15:08:13 +0600

    Https для запросов

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -1,7 +1,7 @@
 function fetchCustom(params, method = 'GET') {
   const apiKey = process.env.REACT_APP_API_KEY;
 
-  return fetch(`http://www.omdbapi.com/?apikey=${apiKey}&${params}`, {
+  return fetch(`https://www.omdbapi.com/?apikey=${apiKey}&${params}`, {
     method,
   });
 }
